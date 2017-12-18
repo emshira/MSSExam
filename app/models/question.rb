502429d9c198dcc8a818edf1f3ca185dfee436c5
Nashira Emery <nashiraemery1@gmail.com>
2017-12-18 18:00:18 -0500

    solving canvas problem

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :subject
-  belongs_to :practitioners
-  belongs_to :patients
+  # belongs_to :practitioners
+
 
   has_one :canvas_question_a
   has_one :canvas_question_b
