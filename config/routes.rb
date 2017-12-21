5c4e389878be420ee3e471e9e20846f0dc83b495
Nashira Emery <nashiraemery1@gmail.com>
2017-12-21 16:21:27 -0500

    gemfile lock merge conflict reolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-Rails.application.routes.draw do
- root 'practitioners#index'
- resources :patients
- resources :practitioners
- resources :questions
- resources :canvas_question_as
- resources :canvas_question_bs
-
- get '/login' => 'sessions#new'
- post '/login' => 'sessions#create'
- get '/logout' => 'sessions#destroy'
-
- get '/landing' => 'practitioners#landing'
- get '/signup' => 'practitioners#new'
- post '/create' => 'practitioners#create'
-
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
 # Rails.application.routes.draw do
 #   resources :patients
 #   resources :practitioners
@@ -54,5 +35,4 @@ get '/signup' => 'practitioners#new'
 post '/create' => 'practitioners#create'
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 8cfd59c92db7d5add9bcf331106ef7855f892b5b
 end
