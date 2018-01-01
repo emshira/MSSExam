2af67c28c913f826235640b8407bedb150ace88a
Nashira Emery <nashiraemery1@gmail.com>
2017-12-31 20:13:58 -0500

    created high graph to represent exam date and correlated score, adedd pateint name above results, added back button to go from results back to questions. On exam/queestion page added forward button with padding. Edited patient summary page, included padding, buttons, change font size. modifiedd the following: application.js for graph, stylesheets sessions.scss, layouts application.html.erb, views patient show.html.erb, views practitioners landing.html.erb, and view questions new.html.erb.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,86 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+$(document).on('turbolinks:load', function() {
+  // Create the chart
+  var seriesData = JSON.parse($("#final_score").val());
+  $('#container').highcharts({
+    chart: {
+      type: 'column',
+      events: {
+        drillup: function (e) {
+          alert('drill Up');
+          console.log(this);
+          console.log(this.options.series[0].name);
+          console.log(this.options.series[0].data[0].name);
+        }
+      }
+    },
+    title: {
+      text: 'Patient MMSE Results'
+    },
+    xAxis: {
+      type: 'Total Score',
+      labels: {
+        enabled: true,
+        formatter: function() {
+          return seriesData[this.value].name
+        }
+      },
+      title: 'hello',
+    },
+    plotOptions: {
+      series: {
+        borderWidth: 0,
+        dataLabels: {
+            enabled: true,
+        }
+      }
+    },
+
+    series: [{
+        name: 'Final Scores',
+        colorByPoint: true,
+        data: seriesData
+
+    }],
+    drilldown: {
+        drillUpButton: {
+            relativeTo: 'spacingBox',
+            position: {
+                y: 0,
+                x: 0
+            },
+            theme: {
+                fill: 'white',
+                'stroke-width': 1,
+                stroke: 'silver',
+                r: 0,
+                states: {
+                    hover: {
+                        fill: '#bada55'
+                    },
+                    select: {
+                        stroke: '#039',
+                        fill: '#bada55'
+                    }
+                }
+            }
+
+        },
+        series: [{
+            id: '',
+            data: [
+                ['', ],
+
+            ]
+        }, {
+            id: '',
+            data: [
+                ['', ],
+                ['', ]
+            ]
+        }]
+    }
+  });
+});
